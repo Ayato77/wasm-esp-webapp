f6d20361d149fedb4c3d62640048d64560f47653
Otoya Nakakaze <otoya.nakakaze@rwth-aachen.de>
2023-02-27 00:33:14 +0100

    Add handling change of formValues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const initialValues = {mailAddress:"", password: ""};
+  const [formValues, setFormValues] = useState();
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]: value});
+    //console.log(formValues)
+  }
+
   return (
     <div className="formContainer">
       <form>
@@ -9,11 +19,21 @@ function App() {
         <div className="uiForm">
           <div className="formField">
             <label>E-mail address</label>
-            <input type={"text"} placeholder={"E-mail address"} name={"mailadress"}/>
+            <input
+                type={"text"}
+                placeholder={"E-mail address"}
+                name={"mailAdress"}
+                onChange={(e) => handleChange(e)}
+            />
           </div>
           <div className="formField">
             <label>Password</label>
-            <input type={"text"} placeholder={"Password"} name={"password"}/>
+            <input
+                type={"text"}
+                placeholder={"Password"}
+                name={"password"}
+                onChange={(e) => handleChange(e)}
+            />
           </div>
         </div>
         <button className={"submitButton"}>Login</button>
