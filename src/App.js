3e44bf5049b29194ea6508588ca2136a991456b1
Otoya Nakakaze <otoya.nakakaze@rwth-aachen.de>
2023-02-27 00:16:17 +0100

    surface of login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formContainer">
+      <form>
+        <h1>Login Form</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>E-mail address</label>
+            <input type={"text"} placeholder={"E-mail address"} name={"mailadress"}/>
+          </div>
+          <div className="formField">
+            <label>Password</label>
+            <input type={"text"} placeholder={"Password"} name={"password"}/>
+          </div>
+        </div>
+        <button className={"submitButton"}>Login</button>
+      </form>
     </div>
   );
 }
